6f1163b1c65e6710e952980e8ef6800f1c9c2dc6
Flavianaal <flavianaandradedelima@gmail.com>
2024-04-30 17:42:27 -0300

    alterando o caminho do arquivo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('projeto1').addEventListener('click', function() {
-    abrirProjeto('file:///C:/Users/Flaviana%20Andrade/OneDrive/Documentos/INTEGRANDOJSNAWEB/projeto_js01/index.html');
+    abrirProjeto('../projeto_js01/index.html');
 });
 
 
