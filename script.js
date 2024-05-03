44ffc12b8086eae4c3d2398a095c7a3af85ea7b2
Flavianaal <flavianaandradedelima@gmail.com>
2024-05-03 15:29:25 -0300

    Novo Projeto

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ document.getElementById('projeto1').addEventListener('click', function() {
     abrirProjeto('https://flavianaal.github.io/Calculadora_programeIO/');
 });
 
+document.getElementById('projeto2').addEventListener('click', function() {
+    abrirProjeto('https://flavianaal.github.io/desafioPokemon/');
+});
+
+
 
 // Função para abrir o projeto em uma nova página
 function abrirProjeto(url) {
